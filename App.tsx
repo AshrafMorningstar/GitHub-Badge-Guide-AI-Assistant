946c32211f4b3a3cdcdbe262098e357e3f53b0c9
Ashraf Morningstar <ashrafmorningstar@gmail.com>
2026-01-03 17:01:58 +0530

    chore(copyright): update headers and license information © 2026

--- App.tsx
+++ App.tsx
@@ -1,3 +1,11 @@
+/*
+ Copyright (c) 2026 Ashraf Morningstar
+ These are personal recreations of existing projects, developed by Ashraf Morningstar
+ for learning and skill development.
+ Original project concepts remain the intellectual property of their respective creators.
+ Repository: https://github.com/AshrafMorningstar
+*/
+
 import React, { useState, useEffect, useMemo } from 'react';
 import BadgeList from './components/BadgeList';
 import AiSidepanel from './components/AiSidepanel';
